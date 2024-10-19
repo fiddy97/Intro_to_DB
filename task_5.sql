6734d100c32cfd2c29ef7a97717a6fb0d069bf0d
Fidel Larry <fidellarry97@gmail.com>
2024-10-20 01:19:12 +0300

    Adding more data

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Use the provided database
+USE alx_book_store;
+
+-- Insert a single row into the customers table
+INSERT INTO customers (customer_id, first_name, last_name, email, address)
+VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
