9fbd0b39a8add16cd0129ddebca158cec9c59702
Fidel Larry <fidellarry97@gmail.com>
2024-10-19 19:21:50 +0300

    Python script that creates database

---
+++ Intro_to_DB/MySQLServer.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+def create_db():
+    try:
+        #Database connection details
+        mydb = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="",
+                )
+        if mydb.is_connected():
+            cursor = connection.cursor()
+
+            cursor.execute(f"CREATE DATABASE IF NOT EXISTS alx_book_store;")
+            print(f"Database alx_book_store created successfully!")
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection.is_connected():
+            connection.close()
+
+if __name__ == "__main__":
+    create_db()
