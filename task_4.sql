a667ce48a4861673699e66f9296b630f5765bb0c
Fidel Larry <fidellarry97@gmail.com>
2024-10-20 01:14:06 +0300

    Print full descriptions

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 -- Use the provided database
 USE alx_book_store;
 
--- Retrieve the full description of the books table
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+-- Retrieve the full description of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
